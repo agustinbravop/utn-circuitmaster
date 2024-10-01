9e07f2d93ea0c86236e743aa0a85b41cff6df9be
Agustín Bravo Pérez <anbravoperez@gmail.com>
2024-09-30 22:17:25 -0300

    agregado `uasyncio` para conectarse a varias terminales de forma concurrente

--- maestro/main.py
+++ maestro/main.py
@@ -1,11 +1,11 @@
 import network
 import socket
 import time
+import uasyncio as asyncio
 import ubinascii
-import gc
 
 
-def connect_to_wifi(ssid, password, max_retries=10):
+async def connect_to_wifi(ssid, password, max_retries=10):
     """Conecta el dispositivo a Wifi. Devuelve la dirección IP y la máscara de subred."""
     # Configurar el WiFi en modo cliente (station)
     wlan = network.WLAN(network.STA_IF)
@@ -20,26 +20,26 @@ def connect_to_wifi(ssid, password, max_retries=10):
         print(".", end="")
         wlan.connect(ssid, password)
         retries += 1
-        time.sleep(2)  # Esperar antes del próximo intento
+        await asyncio.sleep(2)  # Esperar antes del próximo intento
 
-    print()
+    print()  # Termina la cadena de puntos suspensivos
     if wlan.isconnected():
         return wlan.ifconfig()
     else:
         return None
 
 
-def ip_to_bytes(ip_string):
+def ip_to_bytes(ip_string: str):
     """Convertir una dirección IP en formato string a una lista de bytes."""
     return [int(octet) for octet in ip_string.split(".")]
 
 
-def bytes_to_ip(ip_bytes):
+def bytes_to_ip(ip_bytes: list[int]):
     """Convertir una lista de bytes a formato string IP."""
     return ".".join(str(b) for b in ip_bytes)
 
 
-def calculate_broadcast(ip, mask):
+def calculate_broadcast(ip: str, mask: str):
     """Calcular la dirección de broadcast, necesaria para el descubrimiento de red."""
     ip_bytes = ip_to_bytes(ip)
     mask_bytes = ip_to_bytes(mask)
@@ -47,82 +47,103 @@ def calculate_broadcast(ip, mask):
     return bytes_to_ip(broadcast_bytes)
 
 
-def configure_udp_socket():
-    """Configurar un socket UDP para enviar un broadcast de descubrimiento de red."""
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    return udp
-
-
-def get_team_by_name(name):
-    """Busca en el listado de equipos al que tiene el nombre dado."""
-    for team in TEAMS:
-        if team.name == name:
-            return team
+def get_terminal_by_name(name):
+    """Buscar la terminal con el nombre de equipo dado."""
+    for terminal in ALL_TERMINALS:
+        if terminal.name == name:
+            return terminal
     return None
 
 
-def discover_terminals(broadcast_ip, broadcast_port):
-    """Enviar un mensaje de broadcast para descubrir a los controladores terminales."""
-    udp = configure_udp_socket()
+async def discover_terminals(broadcast_ip, broadcast_port):
+    """Enviar un mensaje de broadcast para descubrir a los controladores terminales. Para cada terminal encontrado devuelve su nombre, IP y puerto del servidor TCP."""
+    # Crear un socket UDP
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Configurar el socket UDP para enviar un broadcast
+    udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # No bloquear la ejecución si no hay mensajes
+    udp.setblocking(False)
+
     udp.sendto(b"DISCOVER", (broadcast_ip, broadcast_port))
 
-    terminals = []
-    start_time = time.ticks_ms()  # Empezar el temporizador
+    # Configurar un temporizador con un timeout para el proceso de descubrimiento
+    start_time = time.ticks_ms()
     timeout_ms = 5000
+    found_terminals: list[tuple[Terminal, str, int]] = []
 
     while time.ticks_diff(time.ticks_ms(), start_time) < timeout_ms:
         try:
-            # Intentar leer 1024 bytes del socket UDP.
+            # Intentar leer 1024 bytes del socket UDP
             response, addr = udp.recvfrom(1024)
             if response.startswith(b"TERMINAL"):
-                _, team_name, port = response.decode().split(";")
-                team = get_team_by_name(team_name)
-                team.connect_to_terminal(addr[0], int(port))
-                terminals.append((team, addr[0]))
-                print(f"Terminal encontrado en {addr[0]}: {team}")
+                _, terminal_name, port = response.decode().split(";")
+                terminal = get_terminal_by_name(terminal_name)
+
+                if terminal is None:
+                    print("Error: nombre de terminal no conocido")
+                    continue
+
+                found_terminals.append((terminal, addr[0], int(port)))
+                print(f"Encontrado a {terminal_name} en {addr[0]}:{port}")
         except OSError:
-            # Continuar si no hay mensajes disponibles
-            pass
+            # Ceder el control si no hay mensajes disponibles
+            await asyncio.sleep(0.01)
 
-    if not terminals:
-        print("No se encontraron dispositivos.")
-
-    return terminals
+    udp.close()
+    return found_terminals
 
 
-class Team():
+class Terminal():
     def __init__(self, name):
         self.name = name
 
-    def connect_to_terminal(self, terminal_ip, port):
-        # Crear socket TCP
-        self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    async def connect_to_terminal(self, ip, port):
+        """Conectar (mediante `asyncio`) al server TCP del terminal."""
+        self.reader, self.writer = await asyncio.open_connection(ip, port)
+        print(f"Conectado a {self.name} en {ip}:{port}")
 
-        # Intentar conectarse al servidor TCP del terminal
-        print(f"Conectando al terminal en {terminal_ip}:{port}...")
-        self.tcp.connect((terminal_ip, port))
+    async def get_data(self):
+        """Solicitar datos al servidor TCP del terminal (polling)."""
+        if not self.writer:
+            # Terminal no conectado, no se puede monitorear
+            return
 
-        # Enviar un mensaje al servidor TCP
-        message = "Hola desde el maestro"
-        self.tcp.send(message.encode())
+        try:
+            # Enviar petición al servidor
+            message = "Hola desde el maestro"
+            self.writer.write(message.encode())  # Acumula el mensaje al buffer
+            await self.writer.drain()           # Envía el buffer al stream
 
-        # Recibir la respuesta del servidor
-        data = self.tcp.recv(1024)
-        print(f"Respuesta del servidor: {data.decode()}")
+            # Recibir la respuesta del servidor
+            data = await self.reader.read(1024)
+            print(f"Respuesta del servidor: {data.decode()}")
+        except Exception as e:
+            print(f"Error con {self.name}: {e}")
 
-        self.tcp.close()
+    async def close_connection(self):
+        """Cerrar la conexión con el server TCP del dispositivo terminal."""
+        self.writer.close()
+        await self.writer.wait_closed()
 
 
-TEAMS = [Team("TeoríaDelDescontrol"),
-         Team("ClubTA"),
-         Team("TeamISI"),
-         Team("Colapintos"),
-         Team("Brogramadores"),
-         Team("LosOgata"),
-         Team("MonteCarlo"),
-         Team("Rompecircuitos"),
-         Team("LosFachas")]
+async def poll_terminal_data(terminals: list[Terminal]):
+    """Polling de datos a todos los terminales encontrados."""
+    interval_seconds = 1
+    while True:
+        tasks = [terminal.get_data() for terminal in terminals]
+        await asyncio.gather(*tasks)
+        await asyncio.sleep(interval_seconds)
+
+# Terminales a monitorear en la red (un terminal por cada equipo).
+ALL_TERMINALS = [Terminal("TeoríaDelDescontrol"),
+                 Terminal("ClubTA"),
+                 Terminal("TeamISI"),
+                 Terminal("Colapintos"),
+                 Terminal("Brogramadores"),
+                 Terminal("LosOgata"),
+                 Terminal("MonteCarlo"),
+                 Terminal("Rompecircuitos"),
+                 Terminal("LosFachas")]
 
 # Datos para conectarse a la red WiFi.
 WLAN_SSID = "agus"
@@ -132,9 +153,8 @@ WLAN_PASSWORD = "agustinb"
 BROADCAST_PORT = 10000
 
 
-def monitoring():
-    gc.collect()
-    if_config = connect_to_wifi(WLAN_SSID, WLAN_PASSWORD)
+async def master_monitoring():
+    if_config = await connect_to_wifi(WLAN_SSID, WLAN_PASSWORD)
     if if_config is None:
         print("Error al conectar a la red Wifi.")
         return None
@@ -145,8 +165,20 @@ def monitoring():
     broadcast_ip = calculate_broadcast(ip_address, subnet_mask)
     print("Dirección de broadcast:", broadcast_ip)
 
-    terminals = discover_terminals(broadcast_ip, BROADCAST_PORT)
-    print("a", terminals)
+    print("Descubrimiento terminales...")
+    found_terminals = await discover_terminals(broadcast_ip, BROADCAST_PORT)
+    terminals = [t[0] for t in found_terminals]
+    print("Terminales descubiertas:", [t.name for t in terminals])
+
+    # Conectar el maestro a cada terminal de manera concurrente
+    async_tasks = []
+    for terminal, ip, port in found_terminals:
+        async_tasks.append(terminal.connect_to_terminal(ip, port))
+
+    await asyncio.gather(*async_tasks)
+
+    # Monitorear los terminales encontrados
+    await poll_terminal_data(terminals)
 
 
-monitoring()
+asyncio.run(master_monitoring())
--- terminal/micro_main.py
+++ terminal/micro_main.py
@@ -1,11 +1,10 @@
 import network
 import socket
-import time
+import uasyncio as asyncio
 import ubinascii
-import gc
 
 
-def connect_to_wifi(ssid, password, max_retries=10):
+async def connect_to_wifi(ssid, password, max_retries=10):
     """Conecta el dispositivo a Wifi. Devuelve la dirección IP y la máscara de subred."""
     # Configurar el WiFi en modo cliente (station)
     wlan = network.WLAN(network.STA_IF)
@@ -20,7 +19,7 @@ def connect_to_wifi(ssid, password, max_retries=10):
         print(".", end="")
         wlan.connect(ssid, password)
         retries += 1
-        time.sleep(2)  # Esperar antes del próximo intento
+        await asyncio.sleep(2)  # Esperar antes del próximo intento
 
     print()
     if wlan.isconnected():
@@ -29,32 +28,26 @@ def connect_to_wifi(ssid, password, max_retries=10):
         return None
 
 
-def configure_udp_broadcast_socket():
-    """Configurar socket UDP para escuchar mensajes de descubrimiento."""
+# def configure_tcp_server_socket(port):
+#     """Configurar socket para un servidor TCP."""
+#     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     # `SO_REUSEADDR` permite hacer soft reboot del programa sin pisar el socket activo.
+#     # Ref: https://stackoverflow.com/questions/3229860/what-is-the-meaning-of-so-reuseaddr-setsockopt-option-linux
+#     tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#     tcp.bind(("", port))
+#     tcp.listen()
+#     return tcp
+
+
+async def listen_for_discovery_messages(team_name, port):
+    """Esperar broadcast de descubrimiento del maestro."""
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Escuchar en todas las interfaces de red
     udp.bind(("0.0.0.0", BROADCAST_PORT))
-    return udp
-
-
-def configure_tcp_server_socket(port):
-    """Configurar socket para un servidor TCP."""
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # `SO_REUSEADDR` permite hacer soft reboot del programa sin pisar el socket activo.
-    # Ref: https://stackoverflow.com/questions/3229860/what-is-the-meaning-of-so-reuseaddr-setsockopt-option-linux
-    tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    tcp.bind(("", port))
-    tcp.listen()
-    return tcp
-
-
-def listen_for_discovery_messages(team_name, port):
-    """Esperar broadcast de descubrimiento del maestro."""
-    udp = configure_udp_broadcast_socket()
+    # No bloquear la ejecución si no hay mensajes
+    udp.setblocking(False)
 
     while True:
-        # No bloquear la ejecución si no hay mensajes
-        udp.setblocking(False)
         try:
             msg, addr = udp.recvfrom(1024)
             if msg == b"DISCOVER":
@@ -64,26 +57,33 @@ def listen_for_discovery_messages(team_name, port):
                 print("Respondido con: ", response)
                 return
         except OSError:
-            pass  # No se recibió ningún mensaje, continuar
+            await asyncio.sleep(0.01)  # Ceder control si no hay mensajes
 
 
-def start_tcp_server(port):
-    tcp = configure_tcp_server_socket(port)
+async def start_tcp_server(port):
+    """Iniciar un servidor TCP para que el maestro pueda conectarse."""
+    server = await asyncio.start_server(handle_client, "0.0.0.0", port)
+    await server.wait_closed()
+
+
+async def handle_client(reader, writer):
+    """Manejar la conexión del maestro de forma asincrónica."""
+    addr = writer.get_extra_info("peername")
+    print(f"Conexión establecida con {addr}")
 
     while True:
-        conn, addr = tcp.accept()  # Aceptar conexión entrante
-        print(f"Conexión TCP establecida con {addr}")
+        try:
+            data = await reader.read(1024)
+            print(f"Mensaje recibido: {data.decode()}")
 
-        # Recibir datos del maestro
-        data = conn.recv(1024)
-        print(f"Mensaje recibido: {data.decode()}")
+            response = "Datos recibidos en el terminal."
+            writer.write(response.encode())
+            await writer.drain()
+        except OSError:
+            await asyncio.sleep(0.1)  # Ceder control si no hay mensajes
 
-        # Enviar una respuesta de vuelta
-        response = "Datos recibidos en el terminal."
-        conn.send(response.encode())
-
-        conn.close()  # Cerrar la conexión
-
+    writer.close()
+    await writer.wait_closed()
 
 # Datos para conectarse a la red WiFi.
 WLAN_SSID = "agus"
@@ -101,9 +101,8 @@ sensor_data = {
 }
 
 
-def master_monitoring():
-    gc.collect()
-    if_config = connect_to_wifi(WLAN_SSID, WLAN_PASSWORD)
+async def monitoring_server():
+    if_config = await connect_to_wifi(WLAN_SSID, WLAN_PASSWORD)
     if if_config is None:
         print("Error al conectar a la red Wifi.")
         return None
@@ -112,8 +111,10 @@ def master_monitoring():
     print(f"Conectado! IP: {terminal_ip}, Máscara de subred: {subnet_mask}")
 
     print("Esperando mensajes de descubrimiento y conexiones del maestro...")
-    listen_for_discovery_messages(TEAM_NAME, TCP_SERVER_PORT)
-    start_tcp_server(TCP_SERVER_PORT)
+    await asyncio.gather(
+        listen_for_discovery_messages(TEAM_NAME, TCP_SERVER_PORT),
+        start_tcp_server(TCP_SERVER_PORT)
+    )
 
 
-master_monitoring()
+asyncio.run(monitoring_server())
